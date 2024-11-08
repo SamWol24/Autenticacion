49f82c07830a6e322c8625c48b81425a33875f7d
samir <https://github.com/SamWol24/SupermarketWEB.git>
2024-11-07 23:30:50 -0500

    confirmacion del menu

---
+++ Pages/Subject/Subject.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Autenticacion.Pages
+{
+    public class SubjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
