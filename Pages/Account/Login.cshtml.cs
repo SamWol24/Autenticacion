ae5d27c9e7a0594eab7b355f8e19fa65ad8ca6f2
samir <https://github.com/SamWol24/SupermarketWEB.git>
2024-11-08 01:43:09 -0500

    prueba de asociacion campos de formulario

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,12 +1,21 @@
+using Autenticacion.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.Identity.Client.Platforms.Features.Desktop;
+using Microsoft.Identity.Client;
+
 
 namespace Autenticacion.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public User User { get; set; }
         public void OnGet()
         {
+        }
+        public void OnPost() {
+            Console.WriteLine("User: " + User.Email + " Password : " + User.Password);
         }
     }
 }
