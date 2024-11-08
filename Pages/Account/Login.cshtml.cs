322808af0ca65ab104e8ccac785f693dca0bce5b
samir <https://github.com/SamWol24/SupermarketWEB.git>
2024-11-08 02:21:19 -0500

    prueba de redireccionamiento index

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -14,8 +14,14 @@ namespace Autenticacion.Pages.Account
         public void OnGet()
         {
         }
-        public void OnPost() {
-            Console.WriteLine("User: " + User.Email + " Password : " + User.Password);
+        public async Task<IActionResult> OnPostAsync() {
+           if (!ModelState.IsValid) return Page();
+
+           if (User.Email == "correo@gmail.com" && User.Password == "12345")
+            {
+                return RedirectToPage("/index");
+            }
+           return Page();
         }
     }
 }
