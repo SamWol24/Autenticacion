cd7c537473fff38cdb683074b198374bd964b0d9
Samir Agudelo Curaca <samiragudelo21@gmail.com>
2024-11-09 00:55:59 -0500

    mecanismo para autenticar

--- Pages/Student/Student.cshtml.cs
+++ Pages/Student/Student.cshtml.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Autenticacion.Pages.Student
 {
+    [Authorize]
     public class StudentModel : PageModel
     {
+     
         public void OnGet()
         {
         }
--- Pages/Teacher/Teacher.cshtml.cs
+++ Pages/Teacher/Teacher.cshtml.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Autenticacion.Pages.Shared.Teacher
 {
+    [Authorize]
     public class TeacherModel : PageModel
     {
+      
         public void OnGet()
         {
         }
