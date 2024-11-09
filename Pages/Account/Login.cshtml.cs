d8d749cfd2dba78323d0041fd759d9d6bcf5216a
samir <https://github.com/SamWol24/SupermarketWEB.git>
2024-11-08 20:26:02 -0500

    verificacion de autenticacion

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,8 +1,10 @@
 using Autenticacion.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 //using Microsoft.Identity.Client.Platforms.Features.Desktop;
 using Microsoft.Identity.Client;
+using System.Security.Claims;
 
 
 namespace Autenticacion.Pages.Account
@@ -15,10 +17,23 @@ namespace Autenticacion.Pages.Account
         {
         }
         public async Task<IActionResult> OnPostAsync() {
-           if (!ModelState.IsValid) return Page();
+            if (!ModelState.IsValid) return Page();
 
-           if (User.Email == "correo@gmail.com" && User.Password == "12345")
+            if (User.Email == "correo@gmail.com" && User.Password == "12345")
             {
+                // se recrea los Claim, datos a almacenar en el Cookie
+                var claims = new List<Claim>
+                {
+                 new Claim(ClaimTypes.Name, "admin"),
+                 new Claim(ClaimTypes.Email,User.Email),
+                };
+                // se asocia los clains creados a un nombre de una cookie
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                // se agrega la identidad creada al ClainsPrincipal de la aplicacion
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                // Se registra exitosamente la autenticacion y se crea la cookie en el navegador
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
                 return RedirectToPage("/index");
             }
            return Page();
